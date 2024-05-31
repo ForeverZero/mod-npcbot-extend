0c622301e530bb5d65bc82daa1dbd3c15244e8af
Zou Haodong <821582717@qq.com>
2024-05-31 15:21:52 +0800

    update

--- src/NbeEquipMgr.cpp
+++ src/NbeEquipMgr.cpp
@@ -40,21 +40,24 @@ void NbeEquipMgr::AutoEquip(Player* player, bot_ai* ai)
     }
 
     ItemSubclassArmor subclass = getItemSubclassArmor(ai, level);
-    randomEquipArmorByMasterLevel(ai, BOT_SLOT_HEAD, INVTYPE_HEAD, subclass);
-    randomEquipArmorByMasterLevel(ai, BOT_SLOT_SHOULDERS, INVTYPE_SHOULDERS, subclass);
-    randomEquipArmorByMasterLevel(ai, BOT_SLOT_CHEST, INVTYPE_CHEST, subclass);
-    randomEquipArmorByMasterLevel(ai, BOT_SLOT_WAIST, INVTYPE_WAIST, subclass);
-    randomEquipArmorByMasterLevel(ai, BOT_SLOT_LEGS, INVTYPE_LEGS, subclass);
-    randomEquipArmorByMasterLevel(ai, BOT_SLOT_FEET, INVTYPE_FEET, subclass);
-    randomEquipArmorByMasterLevel(ai, BOT_SLOT_WRIST, INVTYPE_WRISTS, subclass);
-    randomEquipArmorByMasterLevel(ai, BOT_SLOT_HANDS, INVTYPE_HANDS, subclass);
+    AllowableClass ac = getAllowableClass(ai);
+    randomEquipArmorByMasterLevel(ai, BOT_SLOT_HEAD, INVTYPE_HEAD, subclass, ac);
+    randomEquipArmorByMasterLevel(ai, BOT_SLOT_SHOULDERS, INVTYPE_SHOULDERS, subclass, ac);
+    randomEquipArmorByMasterLevel(ai, BOT_SLOT_CHEST, INVTYPE_CHEST, subclass, ac);
+    randomEquipArmorByMasterLevel(ai, BOT_SLOT_WAIST, INVTYPE_WAIST, subclass, ac);
+    randomEquipArmorByMasterLevel(ai, BOT_SLOT_LEGS, INVTYPE_LEGS, subclass, ac);
+    randomEquipArmorByMasterLevel(ai, BOT_SLOT_FEET, INVTYPE_FEET, subclass, ac);
+    randomEquipArmorByMasterLevel(ai, BOT_SLOT_WRIST, INVTYPE_WRISTS, subclass, ac);
+    randomEquipArmorByMasterLevel(ai, BOT_SLOT_HANDS, INVTYPE_HANDS, subclass, ac);
 
-    randomEquipArmorByMasterLevel(ai, BOT_SLOT_BACK, INVTYPE_CLOAK, ITEM_SUBCLASS_ARMOR_MISC);
-    randomEquipArmorByMasterLevel(ai, BOT_SLOT_NECK, INVTYPE_NECK, ITEM_SUBCLASS_ARMOR_MISC);
-    randomEquipArmorByMasterLevel(ai, BOT_SLOT_FINGER1, INVTYPE_FINGER, ITEM_SUBCLASS_ARMOR_MISC);
-    randomEquipArmorByMasterLevel(ai, BOT_SLOT_FINGER2, INVTYPE_FINGER, ITEM_SUBCLASS_ARMOR_MISC);
-    randomEquipArmorByMasterLevel(ai, BOT_SLOT_TRINKET1, INVTYPE_TRINKET, ITEM_SUBCLASS_ARMOR_MISC);
-    randomEquipArmorByMasterLevel(ai, BOT_SLOT_TRINKET2, INVTYPE_TRINKET, ITEM_SUBCLASS_ARMOR_MISC);
+    randomEquipArmorByMasterLevel(ai, BOT_SLOT_BACK, INVTYPE_CLOAK, ITEM_SUBCLASS_ARMOR_MISC, ac);
+    randomEquipArmorByMasterLevel(ai, BOT_SLOT_NECK, INVTYPE_NECK, ITEM_SUBCLASS_ARMOR_MISC, ac);
+    randomEquipArmorByMasterLevel(ai, BOT_SLOT_FINGER1, INVTYPE_FINGER, ITEM_SUBCLASS_ARMOR_MISC, ac);
+    randomEquipArmorByMasterLevel(ai, BOT_SLOT_FINGER2, INVTYPE_FINGER, ITEM_SUBCLASS_ARMOR_MISC, ac);
+    randomEquipArmorByMasterLevel(ai, BOT_SLOT_TRINKET1, INVTYPE_TRINKET, ITEM_SUBCLASS_ARMOR_MISC, ac);
+    randomEquipArmorByMasterLevel(ai, BOT_SLOT_TRINKET2, INVTYPE_TRINKET, ITEM_SUBCLASS_ARMOR_MISC, ac);
+
+    randomEquipWeaponByMasterLevel(ai);
 }
 
 ItemSubclassArmor NbeEquipMgr::getItemSubclassArmor(bot_ai* ai, uint8 level)
@@ -80,7 +83,37 @@ ItemSubclassArmor NbeEquipMgr::getItemSubclassArmor(bot_ai* ai, uint8 level)
     return ITEM_SUBCLASS_ARMOR_CLOTH;
 }
 
-void NbeEquipMgr::randomEquipArmorByMasterLevel(bot_ai* ai, uint8 botSlot, InventoryType inventoryType, ItemSubclassArmor subclass)
+ItemSubclassWeapon NbeEquipMgr::getItemSubclassWeaponByMasterLevel(bot_ai* ai)
+{
+    uint8 botClass = ai->GetBotClass();
+    if (botClass == BOT_CLASS_MAGE || botClass == BOT_CLASS_WARLOCK || botClass == BOT_CLASS_PRIEST || botClass == BOT_CLASS_SHAMAN || botClass == BOT_CLASS_DRUID)
+    {
+        return ITEM_SUBCLASS_WEAPON_MACE2;
+    }
+    if (botClass == BOT_CLASS_WARRIOR || botClass == BOT_CLASS_PALADIN)
+    {
+        uint8 spec = ai->GetSpec();
+        if (spec == BOT_SPEC_WARRIOR_PROTECTION || spec == BOT_SPEC_PALADIN_PROTECTION)
+        {
+            return ITEM_SUBCLASS_WEAPON_SWORD;
+        }
+        else
+        {
+            return ITEM_SUBCLASS_WEAPON_SWORD2;
+        }
+    }
+    if (botClass == BOT_CLASS_DEATH_KNIGHT || botClass == BOT_CLASS_HUNTER)
+    {
+        return ITEM_SUBCLASS_WEAPON_SWORD;
+    }
+    if (botClass == BOT_CLASS_ROGUE)
+    {
+        return ITEM_SUBCLASS_WEAPON_DAGGER;
+    }
+    return ITEM_SUBCLASS_WEAPON_FISHING_POLE;
+}
+
+void NbeEquipMgr::randomEquipArmorByMasterLevel(bot_ai* ai, uint8 botSlot, InventoryType inventoryType, ItemSubclassArmor subclass, AllowableClass allowableClass)
 {
     uint8 toLevel = ai->GetBotOwner()->GetLevel();
     uint8 fromLevel = toLevel - 5;
@@ -88,11 +121,17 @@ void NbeEquipMgr::randomEquipArmorByMasterLevel(bot_ai* ai, uint8 botSlot, Inven
     ObjectGuid botGuid = ai->GetGUID();
     LOG_INFO("server.bot", "random equip bot armor by owner level for player {}", owner->GetPlayerName());
 
+    std::string statSql = getItemModTypeArmorSql(ai);
+
     std::string sql = "select entry, name from item_template";
     sql += " where class = 4 and subclass = {} and InventoryType = {} and RequiredLevel between {} and {}";
+    sql += " and (AllowableClass = -1 or AllowableClass & {} > 0)";
+    sql += " and (stat_type1 in" + statSql + "or stat_type2 in" + statSql + "or stat_type3 in" + statSql + "or stat_type4 in" + statSql
+        + "or stat_type5 in" + statSql + "or stat_type6 in" + statSql + "or stat_type7 in" + statSql + "or stat_type8 in" + statSql
+        +  ")";
     sql += " order by rand() limit 1";
 
-    QueryResult rst = WorldDatabase.Query(sql, subclass, inventoryType, fromLevel, toLevel);
+    QueryResult rst = WorldDatabase.Query(sql, subclass, inventoryType, fromLevel, toLevel, allowableClass);
     if (!rst)
     {
         LOG_WARN("server.bot", "unable to find equip slot {} type {} for bot {}", botSlot, inventoryType, botGuid.GetCounter());
@@ -104,4 +143,81 @@ void NbeEquipMgr::randomEquipArmorByMasterLevel(bot_ai* ai, uint8 botSlot, Inven
     std::string name = field[1].Get<std::string>();
     LOG_INFO("server.bot", "equip {} - {} for bot {}", entry, name, botGuid.GetCounter());
     Equip(owner, ai, entry, botSlot);
+}
+
+void NbeEquipMgr::randomEquipWeaponByMasterLevel(bot_ai* ai)
+{
+
+}
+
+AllowableClass NbeEquipMgr::getAllowableClass(bot_ai* ai)
+{
+    uint8 botClass = ai->GetBotClass();
+    switch (botClass)
+    {
+    case BOT_CLASS_MAGE:
+        return ALLOWABLE_CLASS_MAGE;
+    case BOT_CLASS_PRIEST:
+        return ALLOWABLE_CLASS_PRIEST;
+    case BOT_CLASS_WARLOCK:
+        return ALLOWABLE_CLASS_WARLOCK;
+    case BOT_CLASS_ROGUE:
+        return ALLOWABLE_CLASS_ROUGE;
+    case BOT_CLASS_DRUID:
+        return ALLOWABLE_CLASS_DRUID;
+    case BOT_CLASS_HUNTER:
+        return ALLOWABLE_CLASS_HUNTER;
+    case BOT_CLASS_SHAMAN:
+        return ALLOWABLE_CLASS_SHAMAN;
+    case BOT_CLASS_WARRIOR:
+        return ALLOWABLE_CLASS_WARRIOR;
+    case BOT_CLASS_PALADIN:
+        return ALLOWABLE_CLASS_PALADIN;
+    case BOT_CLASS_DEATH_KNIGHT:
+        return ALLOWABLE_CLASS_DEATH_KNIGHT;
+    default:
+        return ALLOWABLE_CLASS_UNKNOWN;
+    }
+}
+
+std::string NbeEquipMgr::getItemModTypeArmorSql(bot_ai* ai)
+{
+    // HEALER
+    uint8 spec = ai->GetSpec();
+    if (spec == BOT_SPEC_DRUID_RESTORATION || spec == BOT_SPEC_SHAMAN_RESTORATION || spec == BOT_SPEC_PRIEST_HOLY
+        || spec == BOT_SPEC_PRIEST_DISCIPLINE || spec == BOT_SPEC_PALADIN_HOLY)
+    {
+        return "(6, 5, 41, 45)";
+    }
+
+    // TANK
+    if ((spec == BOT_SPEC_DRUID_FERAL && ai->HasRole(BOT_ROLE_TANK)) || spec == BOT_SPEC_WARRIOR_PROTECTION
+        || spec == BOT_SPEC_PALADIN_PROTECTION || spec == BOT_SPEC_DK_BLOOD)
+    {
+        return "(1, 7, 12, 13, 14, 15, 22, 23, 24, 25, 26, 27, 33, 34, 48)";
+    }
+
+    // DPS
+    uint8 cls = ai->GetBotClass();
+    if (cls == BOT_CLASS_PRIEST || cls == BOT_CLASS_WARLOCK || cls == BOT_CLASS_MAGE || cls == BOT_CLASS_SHAMAN
+        ||  spec == BOT_SPEC_DRUID_BALANCE || spec == BOT_SPEC_SHAMAN_ELEMENTAL)
+    {
+        return "(5, 18, 21, 30, 32, 36, 42, 45, 47)";
+    }
+
+    if (cls == BOT_CLASS_HUNTER)
+    {
+        return "(3, 17, 20, 29, 31, 32, 36, 39, 44)";
+    }
+    if (spec == BOT_SPEC_DRUID_FERAL || cls == BOT_CLASS_ROGUE || spec == BOT_SPEC_SHAMAN_ENHANCEMENT)
+    {
+        return "(3, 16, 19, 28, 31, 32, 38, 44)";
+    }
+    if (cls == BOT_CLASS_WARRIOR || cls == BOT_CLASS_PALADIN || cls == BOT_CLASS_DEATH_KNIGHT)
+    {
+        return "(4, 16, 19, 28, 31, 32, 36, 38, 44)";
+    }
+
+    LOG_WARN("server.bot", "getItemModTypeArmorSql failed for bot {}", ai->GetGUID().GetCounter());
+    return "(0)";
 }
--- src/NbeEquipMgr.h
+++ src/NbeEquipMgr.h
@@ -10,7 +10,21 @@
 #include "botcommon.h"
 #include "ItemTemplate.h"
 
-typedef std::map<uint8, uint32> EquipEntries;
+enum AllowableClass
+{
+    ALLOWABLE_CLASS_UNKNOWN = 0,
+
+    ALLOWABLE_CLASS_WARRIOR = 1,
+    ALLOWABLE_CLASS_PALADIN = 2,
+    ALLOWABLE_CLASS_HUNTER = 4,
+    ALLOWABLE_CLASS_ROUGE = 8,
+    ALLOWABLE_CLASS_PRIEST = 16,
+    ALLOWABLE_CLASS_DEATH_KNIGHT = 32,
+    ALLOWABLE_CLASS_SHAMAN = 64,
+    ALLOWABLE_CLASS_MAGE = 128,
+    ALLOWABLE_CLASS_WARLOCK = 256,
+    ALLOWABLE_CLASS_DRUID = 1024
+};
 
 class NbeEquipMgr {
 public:
@@ -22,7 +36,11 @@ public:
     void AutoEquip(Player* player, bot_ai* ai);
 private:
     ItemSubclassArmor getItemSubclassArmor(bot_ai* ai, uint8 level);
-    void randomEquipArmorByMasterLevel(bot_ai* ai, uint8 botSlot, InventoryType inventoryType, ItemSubclassArmor subclass);
+    ItemSubclassWeapon getItemSubclassWeaponByMasterLevel(bot_ai* ai);
+    void randomEquipArmorByMasterLevel(bot_ai* ai, uint8 botSlot, InventoryType inventoryType, ItemSubclassArmor subclass, AllowableClass allowableClass);
+    void randomEquipWeaponByMasterLevel(bot_ai* ai);
+    AllowableClass getAllowableClass(bot_ai* ai);
+    std::string getItemModTypeArmorSql(bot_ai* ai);
 };
 
 #define sNbeEquipMgr NbeEquipMgr::instance()
